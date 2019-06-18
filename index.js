d64eb2b8043045bddbdb336c4c1d6a30b410defe
David Lebee <david@poweredsoft.com>
2019-06-18 08:10:06 -0500

    post api

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
-const schema = require('./schema.js');
+const graphqlHttp = require('express-graphql');
 
+const port = 4000;
 const app = express();
+const schema = require('./schema.js');
+const Chalk = require('chalk');
+const cors = require('cors');
 
-app.use('/graphql', expressGraphQL({
-    schema:schema,
-    graphiql:true
-}));
+//app.use('/graphi',express.static(`${__dirname}/public`)); // we could have just used the `graphiql` option: https://github.com/graphql/express-graphql
 
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+app.use('/graphql', cors(), graphqlHttp(() => ({
+    schema: schema,
+	graphiql: true
+})));
+app.listen(port);
+console.log(`Started on ${Chalk.underline.blue(`http://localhost:${port}/`)}`);
